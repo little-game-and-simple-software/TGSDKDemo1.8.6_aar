d70ea8381696875eab27573ead5821b87ff0eb2a
2439905184 <suchenglong018@126.com>
2021-01-02 10:06:10 +0800

    init

---
+++ app/src/main/java/test/tgsdk/jar/MainActivity.java
@@ -0,0 +1,15 @@
+package test.tgsdk.jar;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity { 
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    //    TGSDK.init();
+    }
+	
+} 
